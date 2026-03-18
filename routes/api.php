79847d2b020a3e3dd7845bf2402966dd89626ffc
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-03-18 17:05:42 +0100

    Remove api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,2 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
-Route::get('/test', function() {
-    return response()->json(['message' => 'L’API répond enfin !']);
-});
-
-Route::post('/register', function() {
-    return response()->json(['message' => 'Route register trouvée !']);
-});
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,14 @@ Route::get('/migrate-db', function () {
     }
 });
 
+Route::get('/test', function() {
+    return response()->json(['message' => 'L’API répond enfin !']);
+});
+
+Route::post('/register', function() {
+    return response()->json(['message' => 'Route register trouvée !']);
+});
+
 // use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
