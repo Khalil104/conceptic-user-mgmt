3c1ab4e624e5a44146bcb6e6e540e4a546daa148
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-03-18 11:21:46 +0100

    Fix: Runtime and Bootstrap Cache path

--- api/index.php
+++ api/index.php
@@ -1,3 +1,34 @@
 <?php
+use Illuminate\Http\Request;
 
-require __DIR__ . "/../public/index.php";
+define('LARAVEL_START', microtime(true));
+
+// Création des dossiers indispensables dans /tmp avant que laravel ne démarre.
+
+$paths = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/sessions',
+    '/tmp/storage/bootstrap/cache'
+];
+
+foreach ($paths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0755, true);
+    }
+}
+
+// 2. On charge l'autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// 3. On initialise l'application 
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 4. On force les chemins (crucial pour vercel)
+$app->useStoragePath('/tmp/storage');
+$app->bind('path.bootstrap', function () {
+    return 'tmp/bootstrap';
+}) ;
+
+// 5. On traite la requête 
+$app->handleRequest(Request::capture());
