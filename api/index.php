9ed3e03b34ba068dcfe659a9c4293a145300c48c
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-03-18 11:40:41 +0100

    Fix: Add index.php

--- api/index.php
+++ api/index.php
@@ -3,32 +3,26 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Création des dossiers indispensables dans /tmp avant que laravel ne démarre.
-
-$paths = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/sessions',
-    '/tmp/storage/bootstrap/cache'
-];
-
-foreach ($paths as $path) {
-    if (!is_dir($path)) {
-        mkdir($path, 0755, true);
-    }
+// 1. Nettoyage et création des dossiers dans /tmp
+$baseTmp = '/tmp/bootstrap/cache';
+if (!is_dir($baseTmp)) {
+    mkdir($baseTmp, 0755, true);
 }
 
-// 2. On charge l'autoloader
-require __DIR__ . '/../vendor/autoload.php';
+// 2. Création des dossiers de storage
+$storagePaths = ['/tmp/storage/framework/views', '/tmp/storage/framework/cache', '/tmp/storage/framework/sessions'];
+foreach ($storagePaths as $path) {
+    if (!is_dir($path)) mkdir($path, 0755, true);
+}
 
-// 3. On initialise l'application 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+// 3. Chargement de l'autoloader
+require __DIR__.'/../vendor/autoload.php';
 
-// 4. On force les chemins (crucial pour vercel)
+// 4. Initialisation de l'application
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// 5. On force les chemins une dernière fois
 $app->useStoragePath('/tmp/storage');
-$app->bind('path.bootstrap', function () {
-    return '/tmp/bootstrap';
-}) ;
+$app->useBootstrapPath('/tmp/bootstrap');
 
-// 5. On traite la requête 
 $app->handleRequest(Request::capture());
