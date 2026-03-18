cd9172a44296f9a5c1d8d1c3b521f40228fd2c78
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-03-18 12:33:58 +0100

    Add migration route

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,25 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DashboardController;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+
+Route::get('/migrate-db', function(){
+    try {
+        // 1. On vérifie la connexion brute
+        DB::connection()->getPdo();
+
+        // 2. On lance les migrations 
+        Artisan::call('migrate', ['--force' => true]);
+
+        // 3. On récupère le résultat pour afficher à l'écran
+        $outpout = Artisan::output();
+
+        return response("✅ Migration réussie ! \nDétails : \n . $output")->header('content-Type', 'text/plain');
+    } catch (\Exception $e) {
+        return response("❌ Erreur lors de la migration :\n\n" . $e->getMessage())->header('Content-Type', 'text/plain');
+    } 
+});
 
 // --- Routes publiques ---
 Route::post('/register', [UserController::class, 'register']);
