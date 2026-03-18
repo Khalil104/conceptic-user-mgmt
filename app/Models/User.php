d369a739d525a8e4321e6bea83fb4e13f14b1463
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-03-18 18:35:20 +0100

    Update Model : : Replace B by b

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\DataBase\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 
