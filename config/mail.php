e2d5cb066e6808d4bd3e84f3ae7fbe5df329244f
Abdoul Rachid <rhdkhalil104@gmail.com>
2026-03-22 14:55:13 +0100

    Add Brevo transporteur

--- config/mail.php
+++ config/mail.php
@@ -53,6 +53,11 @@ return [
             'transport' => 'ses',
         ],
 
+        'brevo' => [
+            'transport' => 'brevo',
+            'dsn' => env('MAILER_DSN'),
+        ],
+
         'postmark' => [
             'transport' => 'postmark',
             // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
